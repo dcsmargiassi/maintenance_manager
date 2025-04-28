dab468eaa5e06d3a3bba9e4d5b50a2084b40619e
dcsmargiassi <danny.smargiassi@gmail.com>
2025-04-28 00:46:49 -0400

    Added safety check before leaving page without saving

--- lib/edit_vehicle_information.dart
+++ lib/edit_vehicle_information.dart
@@ -10,6 +10,7 @@ import 'package:maintenance_manager/auth/auth_state.dart';
 import 'package:maintenance_manager/data/database_operations.dart';
 import 'package:maintenance_manager/helper_functions/format_date.dart';
 import 'package:maintenance_manager/helper_functions/get_user_id.dart';
+import 'package:maintenance_manager/helper_functions/page_navigator.dart';
 import 'package:maintenance_manager/models/vehicle_information.dart';
 import 'package:date_format_field/date_format_field.dart';
 import 'package:provider/provider.dart';
@@ -74,113 +75,139 @@ class _EditVehicleFormState extends State<EditVehicleForm> {
         body: Center(child: CircularProgressIndicator())
         );
     }
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('Edit Vehicle'),
-        ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                controller: vehicleNickNameController,
-                decoration: const InputDecoration(hintText: 'Enter nickname of car'),
-                validator: (value) => value!.isEmpty ? 'Please enter some text' : null,
+      return PopScope(
+        canPop: false,
+        onPopInvoked: (didPop) async {
+          if (didPop) return;
+          bool shouldPop = await showDialog(
+            context: context, 
+            builder: (context) => AlertDialog(
+              title: const Text('Discard changes?'),
+              content: const Text('You have unsaved changes. Are you sure you want to leave?'),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false), // stay on current page
+                child: const Text('Cancel'),
               ),
-              TextFormField(
-                controller: vinController,
-                decoration: const InputDecoration(hintText: 'Enter VIN of car'),
-                validator: (value) => value!.isEmpty ? 'Please enter some text' : null,
-              ),
-              TextFormField(
-                controller: makeController,
-                decoration: const InputDecoration(hintText: 'Enter make of car'),
-                validator: (value) => value!.isEmpty ? 'Please enter some text' : null,
-              ),
-              TextFormField(
-                controller: modelController,
-                decoration: const InputDecoration(hintText: 'Enter model of car'),
-                validator: (value) => value!.isEmpty ? 'Please enter some text' : null,
-              ),
-              TextFormField(
-                controller: versionController,
-                decoration: const InputDecoration(hintText: 'Enter submodel of car'),
-                validator: (value) => value!.isEmpty ? 'Please enter some text' : null,
-              ),
-              TextFormField(
-                controller: yearController,
-                decoration: const InputDecoration(hintText: 'Enter year of car'),
-                validator: (value) => value!.isEmpty ? 'Please enter some text' : null,
-                keyboardType: TextInputType.number,
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                controller: odometerCurrentController,
-                decoration: const InputDecoration(hintText: 'Enter current mileage of car'),
-                validator: (value) => value!.isEmpty ? 'Please enter some text' : null,
-                keyboardType: TextInputType.number,
-              ),
-              DropdownButton<String>(
-                value: selectedUnit,
-                onChanged: (String? newValue) {
-                  setState(() {
-                    selectedUnit = newValue!;
-                  });
-                },
-                items: <String>['Miles Per Hour', 'Kilometers Per Liter']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-              DateFormatField(
-                type: DateFormatType.type4,
-                controller: purchaseDateController,
-                decoration: const InputDecoration(hintText: 'Enter purchase date of car'),
-                onComplete: (date) {
-                  if (date != null) {
-                    purchaseDateController.text = formatDateToString(date);
-                  }
-                },
-              ),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    final updatedVehicle = VehicleInformationModel(
-                      vehicleId: widget.vehicleId,
-                      userId: userId,
-                      vehicleNickName: vehicleNickNameController.text,
-                      vin: vinController.text,
-                      make: makeController.text,
-                      model: modelController.text,
-                      version: versionController.text,
-                      year: int.tryParse(yearController.text) ?? 0,
-                      purchaseDate: purchaseDateController.text,
-                      sellDate: vehicleData!.sellDate,
-                      odometerBuy: vehicleData!.odometerBuy,
-                      odometerSell: vehicleData!.odometerSell,
-                      odometerCurrent: double.tryParse(odometerCurrentController.text) ?? 0.0,
-                      purchasePrice: vehicleData!.purchasePrice,
-                      sellPrice: vehicleData!.sellPrice,
-                    );
-
-                    await VehicleOperations().updateVehicle(updatedVehicle);
-                    _loadVehicleData();
-                    if (!context.mounted) return;
-                    Navigator.pop(context, true); // Return to display vehicle information page
-                  }
-                },
-                child: const Text('Update Vehicle'),
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(true), // Leave page without saving
+                child: const Text('Leave'),
               ),
             ],
+            ),
+          );
+          if (shouldPop == true && mounted) {
+            navigateToSpecificVehiclePage(context, widget.vehicleId);
+          }
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Edit Vehicle'),
           ),
-        ),
+          body: SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    controller: vehicleNickNameController,
+                    decoration: const InputDecoration(hintText: 'Enter nickname of car'),
+                    validator: (value) => value!.isEmpty ? 'Please enter some text' : null,
+                  ),
+                  TextFormField(
+                    controller: vinController,
+                    decoration: const InputDecoration(hintText: 'Enter VIN of car'),
+                    validator: (value) => value!.isEmpty ? 'Please enter some text' : null,
+                  ),
+                  TextFormField(
+                    controller: makeController,
+                    decoration: const InputDecoration(hintText: 'Enter make of car'),
+                    validator: (value) => value!.isEmpty ? 'Please enter some text' : null,
+                  ),
+                  TextFormField(
+                    controller: modelController,
+                    decoration: const InputDecoration(hintText: 'Enter model of car'),
+                    validator: (value) => value!.isEmpty ? 'Please enter some text' : null,
+                  ),
+                  TextFormField(
+                    controller: versionController,
+                    decoration: const InputDecoration(hintText: 'Enter submodel of car'),
+                    validator: (value) => value!.isEmpty ? 'Please enter some text' : null,
+                  ),
+                  TextFormField(
+                    controller: yearController,
+                    decoration: const InputDecoration(hintText: 'Enter year of car'),
+                    validator: (value) => value!.isEmpty ? 'Please enter some text' : null,
+                    keyboardType: TextInputType.number,
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: odometerCurrentController,
+                    decoration: const InputDecoration(hintText: 'Enter current mileage of car'),
+                    validator: (value) => value!.isEmpty ? 'Please enter some text' : null,
+                    keyboardType: TextInputType.number,
+                  ),
+                  DropdownButton<String>(
+                    value: selectedUnit,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        selectedUnit = newValue!;
+                      });
+                    },
+                    items: <String>['Miles Per Hour', 'Kilometers Per Liter']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                  DateFormatField(
+                    type: DateFormatType.type4,
+                    controller: purchaseDateController,
+                    decoration: const InputDecoration(hintText: 'Enter purchase date of car'),
+                    onComplete: (date) {
+                      if (date != null) {
+                        purchaseDateController.text = formatDateToString(date);
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 30),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        final updatedVehicle = VehicleInformationModel(
+                          vehicleId: widget.vehicleId,
+                          userId: userId,
+                          vehicleNickName: vehicleNickNameController.text,
+                          vin: vinController.text,
+                          make: makeController.text,
+                          model: modelController.text,
+                          version: versionController.text,
+                          year: int.tryParse(yearController.text) ?? 0,
+                          purchaseDate: purchaseDateController.text,
+                          sellDate: vehicleData!.sellDate,
+                          odometerBuy: vehicleData!.odometerBuy,
+                          odometerSell: vehicleData!.odometerSell,
+                          odometerCurrent: double.tryParse(odometerCurrentController.text) ?? 0.0,
+                          purchasePrice: vehicleData!.purchasePrice,
+                          sellPrice: vehicleData!.sellPrice,
+                        );
+
+                        await VehicleOperations().updateVehicle(updatedVehicle);
+                        _loadVehicleData();
+                        if (!context.mounted) return;
+                          navigateToSpecificVehiclePage(context, widget.vehicleId);
+                      }
+                    },
+                    child: const Text('Update Vehicle'),
+                  ),
+                ],
+              ),
+            ),
+        )
       )
     );
   }
