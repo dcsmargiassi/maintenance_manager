2e0420f4ec936b40a283a9042cf54038eec1cab3
dcsmargiassi <danny.smargiassi@gmail.com>
2025-05-05 14:49:47 -0400

    Moved global theme to utility file

---
+++ lib/helper_functions/global_theme.dart
@@ -0,0 +1,62 @@
+/* 
+---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.
+ - Code Explanation: Page to store themes to be globally used across the app for all features
+ - Currently Implemented: Light Theme
+---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.---.
+*/
+
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color(0xFF1E88E5),
+      foregroundColor: Colors.white,
+      centerTitle: true,
+      titleTextStyle: TextStyle(fontSize: 26, fontWeight: FontWeight.bold, color: Colors.white)
+    ),
+    colorScheme: const ColorScheme.light(
+      primary: Color(0xFF007BFF),
+      onPrimary: Colors.white,
+      secondary: Color(0xFF616161),
+      onSecondary: Colors.white,
+      background: Colors.white,
+      onBackground: Colors.black,
+      surface: Colors.white,
+      onSurface: Colors.black,
+      error: Colors.red,
+      onError: Colors.white,
+    ),
+    textTheme: const TextTheme(
+      headlineLarge: TextStyle(fontSize: 26, fontWeight: FontWeight.bold, color: Color(0xFF2C2C2C)),
+      titleMedium: TextStyle(fontSize: 18, color: Colors.black),
+      bodyLarge: TextStyle(fontSize: 16, color: Colors.black),
+      labelLarge: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.black),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color(0xFF1E88E5),
+        foregroundColor: Colors.white,
+        elevation: 1,
+        textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      ),
+    ),
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        foregroundColor: const Color(0xFF007BFF),
+        textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+      ),
+    ),
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(
+        foregroundColor: const Color(0xFF007BFF),
+        side: const BorderSide(color: Color(0xFF007BFF), width: 1),
+        textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:maintenance_manager/data/database.dart';
+import 'package:maintenance_manager/helper_functions/global_theme.dart';
 import 'package:maintenance_manager/login_page.dart';
 import 'package:intl/intl_standalone.dart' if (dart.library.html) 'package:intl/intl_browser.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
@@ -48,96 +49,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        final screenSize = MediaQuery.of(context).size;
-        final theme = ThemeData(
-          scaffoldBackgroundColor: Colors.white,
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Color(0xFF1E88E5),
-            foregroundColor: Colors.white,
-            centerTitle: true,
-          ),
-          colorScheme: const ColorScheme(
-            brightness: Brightness.light,
-            primary: Color(0xFF007BFF),
-            onPrimary: Colors.white,
-            secondary: Color(0xFF616161),
-            onSecondary: Colors.white,
-            background: Colors.white,
-            onBackground: Colors.black,
-            surface: Colors.white,
-            onSurface: Colors.black,
-            error: Colors.red,
-            onError: Colors.white,
-          ),
-          textTheme: TextTheme(
-            headlineLarge: TextStyle(
-              fontSize: screenSize.width * 0.06,
-              fontWeight: FontWeight.bold,
-              color: const Color(0xFF2C2C2C),
-            ),
-          titleMedium: TextStyle(
-              fontSize: screenSize.width * 0.04,
-              fontWeight: FontWeight.normal,
-              color: Colors.black,
-            ),
-            bodyLarge: TextStyle(
-              fontSize: screenSize.width * 0.04,
-              fontWeight: FontWeight.normal,
-              color: Colors.black,//const Color(0xFF007BFF),
-            ),
-            labelLarge: TextStyle(
-              fontSize: screenSize.width * 0.04,
-              fontWeight: FontWeight.w500,
-              color: Colors.black,
-              ),
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color(0xFF1E88E5),
-              foregroundColor: Colors.white,
-              elevation: 1,
-              minimumSize: const Size(0, 14),
-              textStyle: TextStyle(
-                fontSize: screenSize.width * 0.045,
-                fontWeight: FontWeight.w600,
-              ),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            ),
-          ),
-          textButtonTheme: TextButtonThemeData(
-            style: TextButton.styleFrom(
-            foregroundColor: const Color(0xFF007BFF),
-            textStyle: TextStyle(
-              fontSize: screenSize.width * 0.045,
-              fontWeight: FontWeight.w500,
-            ),
-            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8),
-              ),
-            ),
-          ),
-          outlinedButtonTheme: OutlinedButtonThemeData(
-            style: OutlinedButton.styleFrom(
-              foregroundColor: const Color(0xFF007BFF),
-              side: const BorderSide(color: Color(0xFF007BFF), width: 1),
-            textStyle: TextStyle(
-              fontSize: screenSize.width * 0.043,
-              fontWeight: FontWeight.w500,
-            ),
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            ),
-          ),
-        );
         return MaterialApp(
           debugShowCheckedModeBanner: false,
-          theme: theme,
+          theme: AppTheme.lightTheme,
           home:  const SignInPage()
         );
       }
