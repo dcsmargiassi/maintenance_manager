18f0e5add9a74ed0075a551f1e3576ce97e00840
dcsmargiassi <danny.smargiassi@gmail.com>
2025-12-29 18:25:54 -0500

    version update

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -109,7 +109,7 @@ class HomePage extends StatelessWidget {
           const Material(
             color: Colors.transparent,
             child: Text(
-              'Version: 0.6.6',
+              'Version: 0.6.7',
               style: TextStyle(fontSize: 12,
               fontWeight: FontWeight.normal,
               color: Colors.grey
